cb643f48c674bb7da033f5ea11a87d9eab139d03
AbuCTF <aburahman918@gmail.com>
2025-10-21 19:52:08 +0530

    update terraform

---
+++ devops/terraform/variables.tf
@@ -0,0 +1,15 @@
+
+variable "aws_region" {
+  description = "AWS region"
+  default     = "eu-north-1"
+}
+
+variable "instance_type" {
+  description = "EC2 instance type"
+  default     = "t3.small"
+}
+
+variable "key_name" {
+  description = "SSH key pair name"
+  default     = "ctfdec2"
+}
